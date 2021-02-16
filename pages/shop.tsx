d17dae192e58cca54a554df15156a2ec4073c0cb
Jonathan Juayong <jonathan.juayong@gmail.com>
2021-02-16 21:35:43 +0800

    added debug component

--- pages/shop.tsx
+++ pages/shop.tsx
@@ -1,12 +1,27 @@
+import { useContext } from "react";
 import ProductCardContainer from "../components/ProductCardContainer";
+import { GlobalContext } from "../lib/context";
 
 interface ShopPageProps {}
 
 const ShopPage: React.FC<ShopPageProps> = () => {
+  const { state } = useContext(GlobalContext);
   return (
-    <main>
-      <ProductCardContainer />
-    </main>
+    <>
+      <main>
+        <div className="debug">
+          <h3>Debug:</h3>
+          <pre>{JSON.stringify(state, null, 2)}</pre>
+        </div>
+        <ProductCardContainer />
+      </main>
+      <style jsx>{`
+        .debug {
+          position: fixed;
+          z-index: 4;
+        }
+      `}</style>
+    </>
   );
 };
 
