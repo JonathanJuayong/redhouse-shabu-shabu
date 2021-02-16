ffe6b9841323869801ede344888c3607efee5563
Jonathan Juayong <jonathan.juayong@gmail.com>
2021-02-16 21:35:54 +0800

    added mock data

---
+++ lib/mock.ts
@@ -0,0 +1,472 @@
+export const mock = [
+  {
+    code: "RH09",
+    name: "Imported Beef Superior",
+    price: 580,
+    category: "Meat",
+    imageSmall:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477876/menu/small/RH63_Tau_Pwe_lmi4um.png",
+    imageBig:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477863/menu/bigger/RH9_Imported_Beef_Superior_-min_rkhba4.png",
+  },
+  {
+    code: "RH10",
+    name: "Imported Beef Supreme",
+    price: 610,
+    category: "Meat",
+    imageSmall:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477879/menu/small/RH10_Imported_Beef_Supreme_knjfsm.png",
+    imageBig:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477861/menu/bigger/RH10_Imported_Beef_Supreme_-min_q6ncq1.png",
+  },
+  {
+    code: "RH11",
+    name: "Imported Beef Premium",
+    price: 400,
+    category: "Meat",
+    imageSmall:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477880/menu/small/RH11_Imported_Beef_Premium_deb7ju.png",
+    imageBig:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477865/menu/bigger/RH11_Imported_Beef_Premium_-min_nofsxq.png",
+  },
+  {
+    code: "RH12",
+    name: "Imported Beef",
+    price: 350,
+    category: "Meat",
+    imageSmall:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477877/menu/small/RH12_Imported_Beef_x8rxeo.png",
+    imageBig:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477865/menu/bigger/RH12_Imported_Beef_-min_ppqujj.png",
+  },
+  {
+    code: "RH13",
+    name: "Imported Lamb",
+    price: 440,
+    category: "Meat",
+    imageSmall:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477877/menu/small/RH13_Imported_Lamb_giaaef.png",
+    imageBig:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477859/menu/bigger/RH13_Imported_Lamb_-min_cg3exk.png",
+  },
+  {
+    code: "RH14",
+    name: "Pork Loin",
+    price: 280,
+    category: "Meat",
+    imageSmall:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477881/menu/small/RH14_Pork_Loin_coagjn.png",
+    imageBig:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477859/menu/bigger/RH14_Pork_Loin_-min_veerhj.png",
+  },
+  {
+    code: "RH33",
+    name: "imported Scallop",
+    price: 200,
+    category: "Seafood",
+    imageSmall:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477879/menu/small/RH33_Imported_Scallop_vwi5a5.png",
+    imageBig:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477866/menu/bigger/RH33_Imported_Scallop_-min_tsjir5.png",
+  },
+  {
+    code: "RH63",
+    name: "Tau Pwe",
+    price: 140,
+    category: "Vegetable",
+    imageSmall:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477876/menu/small/RH63_Tau_Pwe_lmi4um.png",
+    imageBig:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477864/menu/bigger/RH63_Tau_Pwe_-min_isjnc2.png",
+  },
+  {
+    code: "K01",
+    name: "Lobster Ball",
+    price: 200,
+    category: "Hotpot",
+    imageSmall:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477880/menu/small/k01_lobster_ball_b6lkhd.png",
+    imageBig:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477861/menu/bigger/K01_Lobster_Ball_thb4dv.jpg",
+  },
+  {
+    code: "K02",
+    name: "Cuttlefish Ball",
+    price: 200,
+    category: "Hotpot",
+    imageSmall:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477880/menu/small/k02_cuttle_fish_ball_ya75fo.png",
+    imageBig:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477865/menu/bigger/K02_Cuttle_Fish_Ball_axwrqd.jpg",
+  },
+  {
+    code: "K03",
+    name: "Octopus Ball",
+    price: 200,
+    category: "Hotpot",
+    imageSmall:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477880/menu/small/k03_octopus_ball_awn1ch.png",
+    imageBig:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477865/menu/bigger/K03_Octapus_Ball_oxleml.jpg",
+  },
+  {
+    code: "K04",
+    name: "Fu Chow Ball",
+    price: 200,
+    category: "Hotpot",
+    imageSmall:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477877/menu/small/k04_fu_chow_ball_lbqr4h.png",
+    imageBig:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477863/menu/bigger/K04_Fu_Chow_Ball_dglcdt.jpg",
+  },
+  {
+    code: "K05",
+    name: "Fish Ball",
+    price: 200,
+    category: "Hotpot",
+    imageSmall:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477882/menu/small/k05_fish_ball_eva81z.png",
+    imageBig:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477860/menu/bigger/K05_Fish_Ball_ahdyr2.jpg",
+  },
+  {
+    code: "K06",
+    name: "Sea Urchin Ball",
+    price: 200,
+    category: "Hotpot",
+    imageSmall:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477877/menu/small/k06_sea_urchin_dumplings_e5ixo8.png",
+    imageBig:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477864/menu/bigger/K06_Sea_Urchin_Dumplings_gtfces.jpg",
+  },
+  {
+    code: "K07",
+    name: "Tam Choi Fish Ball",
+    price: 200,
+    category: "Hotpot",
+    imageSmall:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477881/menu/small/k07_tam_choi_fish_ball_n3cr0a.png",
+    imageBig:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477862/menu/bigger/K07_Tam_Choi_Fish_Ball_ficocx.jpg",
+  },
+  {
+    code: "K08",
+    name: "Crab Claw",
+    price: 200,
+    category: "Hotpot",
+    imageSmall:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477876/menu/small/k08_crab_claw_supxi8.png",
+    imageBig:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477864/menu/bigger/K08_Crab_Claw_sxkzvt.jpg",
+  },
+  {
+    code: "K09",
+    name: "Scallop Ball",
+    price: 200,
+    category: "Hotpot",
+    imageSmall:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477876/menu/small/k09_scallop_ball_oyg4us.png",
+    imageBig:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477863/menu/bigger/K09_Scallop_Ball_nx5nip.jpg",
+  },
+  {
+    code: "K10",
+    name: "Crab Finger",
+    price: 200,
+    category: "Hotpot",
+    imageSmall:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477879/menu/small/k10_crab_finger_ue9hiw.png",
+    imageBig:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477862/menu/bigger/K10_Crab_Finger_pgys78.jpg",
+  },
+  {
+    code: "K11",
+    name: "Squid Finger",
+    price: 200,
+    category: "Hotpot",
+    imageSmall:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477879/menu/small/k11_squid_finger_ddzopa.png",
+    imageBig:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477860/menu/bigger/K11_Squip_Finger_rldc1p.jpg",
+  },
+  {
+    code: "K12",
+    name: "Kiddie Combination",
+    price: 200,
+    category: "Hotpot",
+    imageSmall:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477881/menu/small/k12_kiddy_combination_kos8tn.png",
+    imageBig:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477864/menu/bigger/K12_Kiddy_Combination_kpogxn.jpg",
+  },
+  {
+    code: "K13",
+    name: "Rice Cake",
+    price: 200,
+    category: "Hotpot",
+    imageSmall:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477876/menu/small/k13_rice_cake_qrvhf9.png",
+    imageBig:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477859/menu/bigger/K13_Rice_Cake_sqeeng.jpg",
+  },
+  {
+    code: "K14",
+    name: "Chikuwa",
+    price: 200,
+    category: "Hotpot",
+    imageSmall:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477877/menu/small/k14_chikuwa_c3fpos.png",
+    imageBig:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477865/menu/bigger/K14_Chikuwa_ccrph7.jpg",
+  },
+  {
+    code: "K15",
+    name: "Fish Tofu",
+    price: 200,
+    category: "Hotpot",
+    imageSmall:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477877/menu/small/k15_fish_tofu_knfbg9.png",
+    imageBig:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477864/menu/bigger/K15_Fish_Tofu_hcqrnj.jpg",
+  },
+  {
+    code: "K16",
+    name: "Mushroom Meat Ball",
+    price: 200,
+    category: "Hotpot",
+    imageSmall:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477879/menu/small/k16_mushroom_meat_ball_okliml.png",
+    imageBig:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477862/menu/bigger/K16_Mushroom_Meat_Ball_yklzhv.jpg",
+  },
+  {
+    code: "K17",
+    name: "Meat Ball",
+    price: 200,
+    category: "Hotpot",
+    imageSmall:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477878/menu/small/k17_meat_ball_ggf9gi.png",
+    imageBig:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477859/menu/bigger/K17_Meat_Ball_bek51w.jpg",
+  },
+  {
+    code: "K18",
+    name: "Black Pepper Meat Ball",
+    price: 200,
+    category: "Hotpot",
+    imageSmall:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477878/menu/small/k18_black_pepper_meat_ball_wb2a26.png",
+    imageBig:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477864/menu/bigger/K18_Black_Pepper_Meat_Ball_d7wcw7.jpg",
+  },
+  {
+    code: "K19",
+    name: "Shrimp Dumpling",
+    price: 200,
+    category: "Hotpot",
+    imageSmall:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477877/menu/small/k19_shrimp_dumpling_qj7ecw.png",
+    imageBig:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477866/menu/bigger/K19_Shrimp_Dumpling_vmgdjd.jpg",
+  },
+  {
+    code: "K20",
+    name: "Egg Dumpling",
+    price: 200,
+    category: "Hotpot",
+    imageSmall:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477880/menu/small/k20_egg_dumpling_bumw6k.png",
+    imageBig:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477860/menu/bigger/K20_Egg_Dumpling_wrcotf.jpg",
+  },
+  {
+    code: "K21",
+    name: "Fish Dumpling",
+    price: 200,
+    category: "Hotpot",
+    imageSmall:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477882/menu/small/k21_fish_dumpling_n3kmjk.png",
+    imageBig:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477859/menu/bigger/K21_Fish_Dumpling_rrng18.jpg",
+  },
+  {
+    code: "K22",
+    name: "Mushroom Liquid Meal",
+    price: 220,
+    category: "Hotpot",
+    imageSmall:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477882/menu/small/k22_mushroom_liquid_meal_xceuff.png",
+    imageBig:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477865/menu/bigger/K22_Mushroom_Liquid_Meal_ou0wse.jpg",
+  },
+  {
+    code: "K23",
+    name: "Cuttlefish Liquid Meal",
+    price: 220,
+    category: "Hotpot",
+    imageSmall:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477880/menu/small/k23_cuttle_fish_liquid_meal_bemwsk.png",
+    imageBig:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477860/menu/bigger/K23_Cuttle_Fish_Liquid_Meal_du9eul.jpg",
+  },
+  {
+    code: "K24",
+    name: "Fish Egg Ball",
+    price: 200,
+    category: "Hotpot",
+    imageSmall:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477877/menu/small/k24_fish_egg_ball_bqdrv7.png",
+    imageBig:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477861/menu/bigger/K24_Fish_Egg_Ball_iasiqu.jpg",
+  },
+  {
+    code: "K25",
+    name: "Beef Ball",
+    price: 200,
+    category: "Hotpot",
+    imageSmall:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477882/menu/small/k25_beef_ball_gfrrkp.png",
+    imageBig:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477862/menu/bigger/K25_Beef_Ball_zldwnw.jpg",
+  },
+  {
+    code: "K26",
+    name: "Crab Leg Stick",
+    price: 200,
+    category: "Hotpot",
+    imageSmall:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477878/menu/small/k26_crab_leg_stick_o8ditx.png",
+    imageBig:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477866/menu/bigger/K26_Crab_Leg_Stick_toaocg.jpg",
+  },
+  {
+    code: "K31",
+    name: "Taro Cake",
+    price: 200,
+    category: "Hotpot",
+    imageSmall:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477881/menu/small/k31_taro_cake_o9ixvm.png",
+    imageBig:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477866/menu/bigger/K31_Taro_Cake_ce6nn0.jpg",
+  },
+  {
+    code: "K32",
+    name: "Taro Fish Ball",
+    price: 200,
+    category: "Hotpot",
+    imageSmall:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477880/menu/small/k32_taro_fish_ball_aonw4f.png",
+    imageBig:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477859/menu/bigger/K32_Taro_Fish_Ball_jjcvrq.jpg",
+  },
+  {
+    code: "K33",
+    name: "Shrimp Soup Bun",
+    price: 200,
+    category: "Hotpot",
+    imageSmall:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477877/menu/small/k33_shrimp_soup_bun_oqarur.png",
+    imageBig:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477866/menu/bigger/K33_Shrimp_Soup_Bun_bcoenc.jpg",
+  },
+  {
+    code: "K35",
+    name: "Fish Egg Sausage",
+    price: 200,
+    category: "Hotpot",
+    imageSmall:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477882/menu/small/k35_fish_egg_sausage_pako82.png",
+    imageBig:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477866/menu/bigger/K35_Fish_Egg_Sausage_itaqqn.jpg",
+  },
+  {
+    code: "K36",
+    name: "Cheese Masi",
+    price: 200,
+    category: "Hotpot",
+    imageSmall:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477878/menu/small/k36_cheese_masi_ikawwo.png",
+    imageBig:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477866/menu/bigger/K36_Cheese_Masi_l6dx8d.jpg",
+  },
+  {
+    code: "K37",
+    name: "Taro Meat Ball",
+    price: 200,
+    category: "Hotpot",
+    imageSmall:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477876/menu/small/k37_taro_meat_ball_f2enbz.png",
+    imageBig:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477862/menu/bigger/K37_Taro_Meat_Ball_tas868.jpg",
+  },
+  {
+    code: "K38",
+    name: "Celery Meat Ball",
+    price: 200,
+    category: "Hotpot",
+    imageSmall:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477879/menu/small/k38_celery_meat_ball_seyovg.png",
+    imageBig:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477861/menu/bigger/K38_Celery_Meat_Ball_neu7nt.jpg",
+  },
+  {
+    code: "K39",
+    name: "Fortune Pouch",
+    price: 200,
+    category: "Hotpot",
+    imageSmall:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477880/menu/small/k39_fortune_pouch_t39mwt.png",
+    imageBig:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477864/menu/bigger/K39_Fortune_Pouch_rivwyh.jpg",
+  },
+  {
+    code: "K40",
+    name: "Tofu Bun",
+    price: 200,
+    category: "Hotpot",
+    imageSmall:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477876/menu/small/k40_tofu_bun_cammxx.png",
+    imageBig:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477860/menu/bigger/K40_Tofu_Bun_wqdij0.jpg",
+  },
+  {
+    code: "T10",
+    name: "Taiwan Sausage",
+    price: 220,
+    category: "Meat",
+    imageSmall:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477881/menu/small/t10_taiwan_sausage_eoqqci.png",
+    imageBig:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477863/menu/bigger/T10_Taiwan_Sausage_emzzvr.jpg",
+  },
+  {
+    code: "SETA",
+    name: "Set A",
+    price: 1350,
+    category: "Set",
+    imageSmall:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477879/menu/small/SET_A_a1xd14.png",
+    imageBig:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477863/menu/bigger/SET_A_z2jhs5.png",
+  },
+  {
+    code: "SETB",
+    name: "Set B",
+    price: 900,
+    category: "Set",
+    imageSmall:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477878/menu/small/SET_B_shsf1g.png",
+    imageBig:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477861/menu/bigger/SET_B_icrscz.png",
+  },
+  {
+    code: "SETC",
+    name: "Set C",
+    price: 600,
+    category: "Set",
+    imageSmall:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477879/menu/small/SET_C_aa33y8.png",
+    imageBig:
+      "https://res.cloudinary.com/taiwanhotpotballs/image/upload/v1613477864/menu/bigger/SET_C_xnbzek.png",
+  },
+];
