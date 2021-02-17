ff5fcdc7294f7ccd562d6322151695b3e852a0e9
Jonathan Juayong <jonathan.juayong@gmail.com>
2021-02-18 00:33:10 +0800

    added redirect

--- next.config.js
+++ next.config.js
@@ -2,4 +2,13 @@ module.exports = {
   images: {
     domains: ["res.cloudinary.com"],
   },
+  async redirects() {
+    return [
+      {
+        source: "/products",
+        destination: "/shop",
+        permanent: true,
+      },
+    ];
+  },
 };
