acef1092a0dead3c51f408733ccd8867cc37a469
Jonathan Juayong <jonathan.juayong@gmail.com>
2021-02-17 13:34:44 +0800

    added empty cart state

--- components/Cart/CartItemContainer.tsx
+++ components/Cart/CartItemContainer.tsx
@@ -1,3 +1,5 @@
+import { Grid, Text } from "@chakra-ui/react";
+import Link from "next/link";
 import { useContext } from "react";
 import { GlobalContext } from "../../lib/context";
 import CartItem from "./CartItem";
@@ -6,17 +8,29 @@ interface CartItemContainerProps {}
 
 const CartItemContainer: React.FC<CartItemContainerProps> = () => {
   const { state } = useContext(GlobalContext);
+  const cartCount = state.cart.length;
   return (
-    <>
-      {state.cart.map((item) => (
-        <CartItem
-          key={item.code}
-          code={item.code}
-          name={item.name}
-          total={item.total}
-        />
-      ))}
-    </>
+    <Grid gap="1em">
+      {cartCount === 0 ? (
+        <>
+          <Text as="h3">Your Cart is Empty</Text>
+          <Text as="p">You can add items by visiting the shop page</Text>
+          <Link href="/shop">
+            <a>Click Here</a>
+          </Link>
+        </>
+      ) : (
+        state.cart.map((item) => (
+          <CartItem
+            key={item.code}
+            code={item.code}
+            name={item.name}
+            total={item.total}
+            imageURL={item.imageURL}
+          />
+        ))
+      )}
+    </Grid>
   );
 };
 
