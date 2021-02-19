8938e22b41166cc376f946b1b287f608d83711fa
Jonathan Juayong <jonathan.juayong@gmail.com>
2021-02-19 17:15:54 +0800

    added head component

--- pages/checkout.tsx
+++ pages/checkout.tsx
@@ -15,6 +15,7 @@ import CheckoutSummary from "../components/CheckoutSummary";
 import { FaChevronLeft, FaChevronUp, FaChevronDown } from "react-icons/fa";
 import { useContext } from "react";
 import { GlobalContext } from "../lib/context";
+import Head from "next/head";
 
 interface CheckoutPageProps {}
 
@@ -28,50 +29,65 @@ const CheckoutPage: React.FC<CheckoutPageProps> = () => {
   const btnIcon = !isOpen ? <FaChevronUp /> : <FaChevronDown />;
   if (cart.length === 0)
     return (
-      <Grid
-        h="100vh"
-        margin="0 auto"
-        alignItems="center"
-        alignContent="center"
-        justifyContent="center"
-        gap="1em"
-        w="30vw"
-      >
-        <Text>There are no items in your cart</Text>
-        <Button colorScheme="green" onClick={() => router.push("/")}>
-          Go back to shop
-        </Button>
-      </Grid>
+      <>
+        <Head>
+          <title>Checkout | Redhouse Shabu-Shabu</title>
+        </Head>
+        <Grid
+          h="100vh"
+          margin="0 auto"
+          alignItems="center"
+          alignContent="center"
+          justifyContent="center"
+          gap="1em"
+          w="30vw"
+        >
+          <Text>There are no items in your cart</Text>
+          <Button colorScheme="green" onClick={() => router.push("/")}>
+            Go back to shop
+          </Button>
+        </Grid>
+      </>
     );
   return (
-    <Grid
-      maxW="70vw"
-      margin="0 auto"
-      py="4em"
-      gap="1em"
-      gridTemplateColumns="repeat(auto-fit, minmax(34vw, 1fr))"
-    >
-      <Grid alignContent="start" gap="1em">
-        <Button rightIcon={btnIcon} variant="link" onClick={onToggle} w="100%">
-          {btnText}
-        </Button>
-        <Collapse in={!isOpen} animateOpacity>
-          <Box>
-            <CheckoutSummary />
-          </Box>
-        </Collapse>
+    <>
+      <Head>
+        <title>Checkout | Redhouse Shabu-Shabu</title>
+      </Head>
+      <Grid
+        maxW="70vw"
+        margin="0 auto"
+        py="4em"
+        gap="1em"
+        gridTemplateColumns="repeat(auto-fit, minmax(34vw, 1fr))"
+      >
+        <Grid alignContent="start" gap="1em">
+          <Button
+            rightIcon={btnIcon}
+            variant="link"
+            onClick={onToggle}
+            w="100%"
+          >
+            {btnText}
+          </Button>
+          <Collapse in={!isOpen} animateOpacity>
+            <Box>
+              <CheckoutSummary />
+            </Box>
+          </Collapse>
+        </Grid>
+        <Grid gap="2em">
+          <CheckoutForm />
+          <Button
+            leftIcon={<FaChevronLeft />}
+            onClick={() => router.push("/")}
+            variant="link"
+          >
+            Go back to shop
+          </Button>
+        </Grid>
       </Grid>
-      <Grid gap="2em">
-        <CheckoutForm />
-        <Button
-          leftIcon={<FaChevronLeft />}
-          onClick={() => router.push("/")}
-          variant="link"
-        >
-          Go back to shop
-        </Button>
-      </Grid>
-    </Grid>
+    </>
   );
 };
 
