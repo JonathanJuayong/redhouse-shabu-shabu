2e1d42c4af6a05206f28fb1de3bede9b70e1696b
Jonathan Juayong <jonathan.juayong@gmail.com>
2021-02-21 00:39:49 +0800

    added functions to create products in firestore

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,3 +1,6 @@
+import { db } from "./firebase";
+import { mock } from "./mock";
+
 export const saveToLocalStorage = (data, name: string) => {
   const json = JSON.stringify(data);
   localStorage.setItem(name, json);
@@ -14,4 +17,35 @@ export const loadLocalStorage = (name: string) => {
 
 export const clearLocalStorage = (name: string) => {
   if (localStorage.getItem(name)) localStorage.setItem(name, "");
+};
+
+export const uploadAllMockToFirestore = async () => {
+  console.log("uploading");
+  try {
+    const ref = db.collection("products").doc("all");
+    await ref.set({
+      items: mock,
+    });
+    console.log("done");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const uploadEachMockToFirestore = () => {
+  try {
+    for (const { code, name, price, category, imageBig, imageSmall } of mock) {
+      const ref = db.collection("products").doc(code);
+      ref.set({
+        code,
+        name,
+        price,
+        category,
+        imageBig,
+        imageSmall,
+      });
+    }
+  } catch (error) {
+    console.log(error);
+  }
 };
