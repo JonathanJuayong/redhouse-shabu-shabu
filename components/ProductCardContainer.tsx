461b849b9c4c4740a54dc9c57064ddeecefcc3a7
Jonathan Juayong <jonathan.juayong@gmail.com>
2021-02-26 02:57:58 +0800

    added swipe capabilty

--- components/ProductCardContainer.tsx
+++ components/ProductCardContainer.tsx
@@ -1,5 +1,8 @@
-import { Box, Grid, Text } from "@chakra-ui/react";
+import { Box, Center, Grid, IconButton, Text } from "@chakra-ui/react";
 import ProductCard from "./ProductCard";
+import { MdChevronLeft, MdChevronRight } from "react-icons/md";
+import { useEffect, useRef, useState } from "react";
+import { useSwipeable } from "react-swipeable";
 
 interface ProductCardContainerProps {
   category;
@@ -10,37 +13,98 @@ const ProductCardContainer: React.FC<ProductCardContainerProps> = ({
   category,
   products,
 }) => {
+  const productCodes = products.map((product) => product.code);
+  const [currentProductIndex, setCurrentProductIndex] = useState(0);
+  const focusNextProduct = () => {
+    if (currentProductIndex === productCodes.length - 1) return;
+    setCurrentProductIndex((prev) => prev + 1);
+  };
+  const focusPreviousProduct = () => {
+    if (currentProductIndex === 0) return;
+    setCurrentProductIndex((prev) => prev - 1);
+  };
+  const swipeHandlers = useSwipeable({
+    onSwipedLeft: focusNextProduct,
+    onSwipedRight: focusPreviousProduct,
+    trackTouch: true,
+    trackMouse: true,
+  });
+  const selectedProductOpacity = (code) => {
+    return code === productCodes[currentProductIndex] ? "1" : "0.35";
+  };
+  useEffect(() => {
+    document.getElementById(productCodes[currentProductIndex]).scrollIntoView({
+      behavior: "smooth",
+      block: "nearest",
+      inline: "center",
+    });
+  }, [currentProductIndex]);
   return (
-    <Grid gap="2em" mb="6em" position="relative">
-      <Text justifySelf="center">{category}</Text>
+    <Box mb="8em" position="relative" maxW={["98vw", , , "65vw"]} m="0 auto">
+      <Center mb="2em">{category}</Center>
+      {products.length > 1 && (
+        <IconButton
+          zIndex="3"
+          display={["flex", , , "none"]}
+          colorScheme="green"
+          borderRadius="50%"
+          position="absolute"
+          top="30%"
+          left="10px"
+          aria-label="left button"
+          icon={<MdChevronLeft />}
+          onClick={focusPreviousProduct}
+        />
+      )}
+      {products.length > 1 && (
+        <IconButton
+          zIndex="3"
+          display={["flex", , , "none"]}
+          colorScheme="green"
+          borderRadius="50%"
+          position="absolute"
+          top="30%"
+          right="10px"
+          aria-label="right button"
+          icon={<MdChevronRight />}
+          onClick={focusNextProduct}
+        />
+      )}
       <Grid
+        {...swipeHandlers}
         gap={["1em", , , "2em"]}
         margin="0 auto"
-        pb="2em"
+        pb="6em"
         maxW={["98vw", , , "65vw"]}
         justifyContent={["start", , , "center"]}
+        overflowX="hidden"
+        gridAutoFlow={["column", , , "row"]}
         gridTemplateColumns={[
-          `30px repeat(${products.length}, 230px) 30px`,
+          `4em repeat(${products.length}, 230px) 4em`,
           ,
           ,
           `repeat(auto-fill, 230px)`,
         ]}
-        overflowX="auto"
-        gridAutoFlow={["column", , , "row"]}
       >
         <Box display={["block", , , "none"]}></Box>
         {products.map((product) => (
-          <ProductCard
+          <Box
+            id={product.code}
             key={product.code}
-            code={product.code}
-            name={product.name}
-            imageURL={product.imageSmall}
-            price={product.price}
-          />
+            opacity={[selectedProductOpacity(product.code), , , 1]}
+            transition="opacity .3s"
+          >
+            <ProductCard
+              code={product.code}
+              name={product.name}
+              imageURL={product.imageSmall}
+              price={product.price}
+            />
+          </Box>
         ))}
         <Box display={["block", , , "none"]}></Box>
       </Grid>
-    </Grid>
+    </Box>
   );
 };
 
