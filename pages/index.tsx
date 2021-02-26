b435107f9e0df815cbdbb096aaf2c555d9dc71aa
Jonathan Juayong <jonathan.juayong@gmail.com>
2021-02-26 18:41:01 +0800

    made productCardContainer into dynamic import

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,28 @@
 import {
+  Box,
   Modal,
   ModalBody,
   ModalCloseButton,
   ModalContent,
   ModalOverlay,
+  Spinner,
 } from "@chakra-ui/react";
 import { useRouter } from "next/dist/client/router";
 import { useContext, useEffect } from "react";
-import ProductCardContainer from "../components/ProductCardContainer";
 import ProductDescription from "../components/ProductDescription";
 import { GlobalContext } from "../lib/context";
 import Head from "next/head";
 import { useFirestore } from "../lib/hooks";
 import { getCategories } from "../lib/utils";
+import dynamic from "next/dynamic";
+import Hero from "../components/Hero";
+
+const ProductCardContainer = dynamic(
+  () => import("../components/ProductCardContainer"),
+  {
+    loading: () => <Spinner />,
+  }
+);
 
 interface HomePageProps {
   products;
@@ -44,19 +54,21 @@ const HomePage: React.FC<HomePageProps> = ({ products }) => {
           {/* <pre>{JSON.stringify(state.user, null, 2)}</pre> */}
           {/* <pre>{JSON.stringify(categories, null, 2)}</pre> */}
         </div>
-        {categories.map((category) => {
-          const categorizedProduct = products.filter(
-            (product) => product.category === category
-          );
-          return (
-            <ProductCardContainer
-              key={category}
-              category={category}
-              products={categorizedProduct}
-            />
-          );
-        })}
       </main>
+      <Hero />
+      <Box id="shop"></Box>
+      {categories.map((category) => {
+        const categorizedProduct = products.filter(
+          (product) => product.category === category
+        );
+        return (
+          <ProductCardContainer
+            key={category}
+            category={category}
+            products={categorizedProduct}
+          />
+        );
+      })}
       <Modal isOpen={!!code} onClose={onClose} size="6xl">
         <ModalOverlay />
         <ModalContent>
@@ -78,7 +90,7 @@ const HomePage: React.FC<HomePageProps> = ({ products }) => {
 
 export default HomePage;
 
-export const getServerSideProps = async (context) => {
+export const getStaticProps = async (context) => {
   const { getAllProducts } = useFirestore();
   const products = await getAllProducts();
   return {
