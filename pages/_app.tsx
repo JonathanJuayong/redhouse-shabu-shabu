a0923f2eb40fe9054c4c78645cec4129cc431f71
Jonathan Juayong <jonathan.juayong@gmail.com>
2021-02-28 01:15:32 +0800

    added favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,6 +15,31 @@ const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
           href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&family=Lusitana:wght@400;700&display=swap"
           rel="stylesheet"
         />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+        <link
+          rel="mask-icon"
+          href="/favicon/safari-pinned-tab.svg"
+          color="#5bbad5"
+        />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
       <ChakraProvider resetCSS theme={theme}>
         <GlobalContextProvider>
